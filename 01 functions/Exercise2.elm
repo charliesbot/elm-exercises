f3540e8c62a385fd2dfd1cf5000c2bd79616617d
Charlie <carlyeah@me.com>
2016-10-02 00:12:11 -0500

    exercise 2

---
+++ 01 functions/Exercise2.elm
@@ -0,0 +1,25 @@
+module Main exposing (..)
+
+import Html
+import String
+
+
+uppercase maxLength name =
+    if String.length name > maxLength then
+        String.toUpper name
+    else
+        name
+
+
+main =
+    let
+        name =
+            "CharlieCharlie"
+
+        length =
+            String.length name
+    in
+        (uppercase 10 name)
+            ++ " - name length: "
+            ++ (toString length)
+            |> Html.text
